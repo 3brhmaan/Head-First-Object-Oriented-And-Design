20723b88a24245f93941f0134175cc7688bd4171
Abdullrhman Elsayed <Abdulllrhmannn@gmail.com>
2024-02-18 21:55:14 +0200

    Version 1 of the Dog Door : - after knowing the basic information from the customer , just build the dog door

---
+++ HeadFirstObjectOrientedAndDesign/CH2/DogDoor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadFirstObjectOrientedAndDesign.CH2
+{
+	public class DogDoor
+	{
+        bool open { get; set; }
+
+        public DogDoor() =>
+            this.open = false;
+        public void Open()
+        {
+            Console.WriteLine("The dog door opens");
+            open = true;
+        }
+		public void Close()
+		{
+			Console.WriteLine("The dog door Closes");
+			open = false;
+		}
+        public bool IsOpen() =>
+             open;
+	}
+}
--- HeadFirstObjectOrientedAndDesign/Program.cs
+++ HeadFirstObjectOrientedAndDesign/Program.cs
@@ -1,50 +1,29 @@
 ﻿using HeadFirstObjectOrientedAndDesign.CH1;
+using HeadFirstObjectOrientedAndDesign.CH2;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Reflection;
 
 namespace HeadFirstObjectOrientedAndDesign
 {
     internal class Program
     {
-        public static void initializeInventory(Inventory inventory)
-        {
-            GuitarSpec spec = new(Builder.FENDER,"Stratocastor",
-                         CH1.Type.ELECTRIC,Wood.ALDER, Wood.ALDER);
-
-            inventory.addGuitar("V95693",1499.95, spec);
-            
-            inventory.addGuitar("V9512",1549.95, spec);
-        }
         static void Main(string[] args)
         {
-            Inventory inventory = new Inventory();
-            initializeInventory(inventory);
+			DogDoor door = new DogDoor();
+			Remote remote = new Remote(door);
 
-			GuitarSpec whatErinLikes =
-                    new GuitarSpec(Builder.FENDER, "Stratocastor", CH1.Type.ELECTRIC,
-                    Wood.ALDER, Wood.ALDER);
+			Console.WriteLine("Fido barks to go outside...");
+			remote.PressButton();
 
-			List<Guitar> matchingGuitars = inventory.search(whatErinLikes);
+            Console.WriteLine("\nFido has gone outside...");
+			remote.PressButton();
 
-            if (matchingGuitars.Any())
-            {
-                Console.WriteLine("Erin, you might like these guitars:");
+            Console.WriteLine("\nFido’s all done...");
+			remote.PressButton();
 
-                foreach(Guitar guitar in matchingGuitars)
-                {
-					GuitarSpec spec = guitar.getSpec();
-					Console.WriteLine("We have a " +
-					spec.getBuilder() + " " + spec.getModel() + " " +
-					spec.getType() + " guitar:\n " +
-					spec.getBackWood() + " back and sides,\n " +
-					spec.getTopWood() + " top.\n You can have it for only $" +
-                    guitar.getPrice() + "!\n----") ;
-                }
-            }
-            else
-            {
-                Console.WriteLine("Sorry, Erin, we have nothing for you.") ;
-            }
-        }
+            Console.WriteLine("\nFido’s back inside...");
+			remote.PressButton();
+		}
     }
 }
